19f81ae62af87b0b14aed8e8a84cede5575bee54
atomofiron <atomofiron@gmail.com>
2019-04-04 23:15:28 +0300

    show_touches added into command template

--- app/src/main/java/io/atomofiron/devtiles/service/ShowTapsService.java
+++ app/src/main/java/io/atomofiron/devtiles/service/ShowTapsService.java
@@ -7,7 +7,7 @@ import io.atomofiron.devtiles.util.Cmd;
 
 @SuppressLint("NewApi, DefaultLocale")
 public class ShowTapsService extends BaseService {
-    private static final String CONTENT_TEMPLATE = "su -c content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:%d";
+    private static final String CONTENT_TEMPLATE = "su -c content insert --uri content://settings/system --bind name:s:%s --bind value:i:%d";
     private static final String KEY = "show_touches";
     private static final int ENABLE = 1;
     private static final int DISABLE = 0;
@@ -22,7 +22,7 @@ public class ShowTapsService extends BaseService {
         } catch (SecurityException e) {
             log("exc: " + e.toString());
 
-            success = Cmd.run(String.format(CONTENT_TEMPLATE, value));
+            success = Cmd.run(String.format(CONTENT_TEMPLATE, KEY, value));
         } catch (Exception e) {
             log("exc: " + e.toString());
         }
