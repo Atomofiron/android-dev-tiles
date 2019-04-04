bd1180a9ee6581ddac32aa2fc49f66c33f9bdeab
atomofiron <atomofiron@gmail.com>
2019-04-04 22:15:19 +0300

    'sleep' removed

--- app/src/main/java/io/atomofiron/devtiles/service/BoundsService.java
+++ app/src/main/java/io/atomofiron/devtiles/service/BoundsService.java
@@ -7,7 +7,7 @@ import io.atomofiron.devtiles.util.Result;
 
 @SuppressLint("NewApi")
 public class BoundsService extends BaseService {
-    private static final String SET_PROP = "su -c setprop debug.layout %1$s && sleep 3; service call activity 1599295570 1>/dev/null";
+    private static final String SET_PROP = "su -c setprop debug.layout %1$s && service call activity 1599295570 1>/dev/null";
     private static final String GET_PROP = "getprop debug.layout";
 
     private static final String TRUE = "true";
