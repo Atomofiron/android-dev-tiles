a9bc6b93bae64912cbcbc7f28c50e80116fe6577
atomofiron <atomofiron@gmail.com>
2019-04-07 19:01:49 +0300

    MainActivity replaced with AdbTcpActivity

--- app/src/main/java/io/atomofiron/devtiles/AdbTcpActivity.java
+++ app/src/main/java/io/atomofiron/devtiles/AdbTcpActivity.java
@@ -3,10 +3,10 @@ package io.atomofiron.devtiles;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class AdbTcpActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_adbtcp);
     }
 }
