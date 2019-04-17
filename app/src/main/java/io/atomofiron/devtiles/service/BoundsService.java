e45726ce7f85d6f71a4de61fee162150e1eec952
atomofiron <atomofiron@gmail.com>
2019-04-17 23:39:09 +0300

    PropertiesService added for BoundsService

--- app/src/main/java/io/atomofiron/devtiles/service/BoundsService.java
+++ app/src/main/java/io/atomofiron/devtiles/service/BoundsService.java
@@ -1,49 +1,32 @@
 package io.atomofiron.devtiles.service;
 
-import android.os.SystemProperties;
-
 import io.atomofiron.devtiles.R;
 import io.atomofiron.devtiles.util.Result;
 
-public class BoundsService extends BaseService {
-    private static final String PROP = "debug.layout";
-    private static final String SET_PROP = "su -c setprop " + PROP + " %1$s && service call activity 1599295570 1>/dev/null";
-    private static final String GET_PROP = "getprop " + PROP;
-
-    private static final String TRUE = "true";
-    private static final String FALSE = "false";
+public class BoundsService extends PropertiesService {
 
     {
-        needSu = true;
         unavailableIconResId = R.drawable.ic_qs_layout_bounds_unavailable;
     }
 
     @Override
-    public void onClick(boolean isActive) {
-        String enable = isActive ? FALSE : TRUE;
-
-        try {
-            SystemProperties.set(PROP, enable);
-        } catch (Exception e) {
-            log("exc: " + e.toString());
-
-            updateTile(isActive ? State.INACTIVATING : State.ACTIVATING);
-
-            run(SU_CHECK, String.format(SET_PROP, enable), GET_PROP);
-        }
+    protected String getProp() {
+        return "debug.layout";
     }
 
     @Override
-    protected void onUpdate() {
-        try {
-            String state = SystemProperties.get(PROP);
-            log("state: " + state);
-            updateTile(Boolean.parseBoolean(state) ? State.ACTIVE : State.INACTIVE);
-        } catch (Exception e) {
-            log("exc: " + e.toString());
+    protected String getSetterCommandSuffix() {
+        return " && service call activity 1599295570";
+    }
 
-            run(GET_PROP);
-        }
+    @Override
+    protected String getValueForState(boolean activate) {
+        return activate ? TRUE : FALSE;
+    }
+
+    @Override
+    protected boolean getStateForValue(String value) {
+        return Boolean.parseBoolean(value);
     }
 
     @Override
---
+++ app/src/main/java/io/atomofiron/devtiles/service/PropertiesService.java
@@ -0,0 +1,62 @@
+package io.atomofiron.devtiles.service;
+
+import android.os.SystemProperties;
+
+public abstract class PropertiesService extends BaseService {
+    private final String PROP;
+    private final String SET_PROP;
+    private final String GET_PROP;
+
+    protected static final String TRUE = "true";
+    protected static final String FALSE = "false";
+
+    {
+        PROP = getProp();
+        SET_PROP = "su -c setprop " + PROP + " %s %s";
+        GET_PROP = "getprop " + PROP;
+    }
+
+    protected abstract String getProp();
+
+    protected String getSetterCommandSuffix() {
+        return "";
+    }
+
+    protected abstract String getValueForState(boolean activate);
+
+    protected abstract boolean getStateForValue(String value);
+
+    @Override
+    public final void onClick(boolean isActive) {
+        String value = getValueForState(!isActive);
+
+        try {
+            SystemProperties.set(PROP, value);
+
+            needSu = false;
+        } catch (Exception e) {
+            log("exc: " + e.toString());
+
+            needSu = true;
+
+            updateTile(isActive ? State.INACTIVATING : State.ACTIVATING);
+
+            run(String.format(SET_PROP, value, getSetterCommandSuffix()), GET_PROP);
+        }
+    }
+
+    @Override
+    protected void onUpdate() {
+        log("onUpdate()");
+
+        try {
+            String state = SystemProperties.get(PROP);
+            log("state: " + state);
+            updateTile(getStateForValue(state) ? State.ACTIVE : State.INACTIVE);
+        } catch (Exception e) {
+            log("exc: " + e.toString());
+
+            run(GET_PROP);
+        }
+    }
+}
