ec989d9538a80aab01d61faf8b2ecaeac2c3797e
atomofiron <atomofiron@gmail.com>
2019-07-06 00:10:42 +0300

    logging method added

--- app/src/main/java/io/atomofiron/devtiles/I.java
+++ app/src/main/java/io/atomofiron/devtiles/I.java
@@ -8,4 +8,8 @@ public class I {
     public static void log(String s) {
         Log.e("atomofiron", s);
     }
+
+    public static void log(Object o, String s) {
+        log(String.format("[%s] %s", o.getClass().getSimpleName(), s));
+    }
 }
